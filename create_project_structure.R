f9b5e1ff15c3ec751b574d0958f280f00e0e4a29
Mike Lavender <lmike92@gmail.com>
2023-09-01 10:45:55 -0600

    commit rendering project

---
+++ create_project_structure.R
@@ -0,0 +1,10 @@
+if(!dir.exists("00-rawdata")) { dir.create("00-rawdata")}
+if(!dir.exists("01-scripts")) { dir.create("01-scripts")}
+if(!dir.exists("01-scripts/r_scripts")) { dir.create("01-scripts/r_scripts")}
+if(!dir.exists("01-scripts/RMD")) { dir.create("01-scripts/RMD")}
+if(!dir.exists("02-outdata")) { dir.create("02-outdata")}
+if(!dir.exists("03-figs")) { dir.create("03-figs")}
+if(!dir.exists("04-manuscript")) { dir.create("04-manuscript")}
+if(!dir.exists("04-manuscript/rendered")) { dir.create("04-manuscript/rendered")}
+
+# file.remove("create_project_structure.R")
