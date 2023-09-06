9f16fb5ba27a55bf087fb0e380cf3668b529f1f7
Mike Lavender <lmike92@gmail.com>
2023-09-06 15:27:11 -0600

    - added comments to create_project_structure.R script

--- create_project_structure.R
+++ create_project_structure.R
@@ -1,6 +1,7 @@
 # creates the desired folder structure for the RProject and adds blank README
 # files to each folder.
 
+# folders first
 if(!dir.exists("00-rawdata")) { dir.create("00-rawdata")}
 if(!dir.exists("01-scripts")) { dir.create("01-scripts")}
 if(!dir.exists("01-scripts/r_scripts")) { dir.create("01-scripts/r_scripts")}
@@ -10,7 +11,7 @@ if(!dir.exists("03-figs")) { dir.create("03-figs")}
 if(!dir.exists("04-manuscript")) { dir.create("04-manuscript")}
 if(!dir.exists("04-manuscript/rendered")) { dir.create("04-manuscript/rendered")}
 
-
+# then files
 if(!file.exists("README.md")) {file.create("README.md")}
 if(!file.exists("00-rawdata/_README.md")) {file.create("00-rawdata/_README.md")}
 if(!file.exists("00-rawdata/DATA-DICTIONARY.md")) {file.create("00-rawdata/DATA-DICTIONARY.md")}
