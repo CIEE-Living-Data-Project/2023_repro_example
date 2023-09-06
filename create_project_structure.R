5c2106bb145e802e8b2f3478b496f629f856acee
Mike Lavender <lmike92@gmail.com>
2023-09-06 10:31:15 -0600

    - Updated create_project_structure.R script to create README.md files with preceeding _ to be consistent with Jason's spec. - Refactored appropriate files to be consistent with above changes - refactored bib file to be "my-library.bib" - added code to create empty "my-library.bib" in manuscript folder -added code to create empty "manuscript.RMD" file in manuscript folder.

--- create_project_structure.R
+++ create_project_structure.R
@@ -2,22 +2,24 @@
 # files to each folder.
 
 if(!dir.exists("00-rawdata")) { dir.create("00-rawdata")}
-if(!file.exists("00-rawdata/README.MD")) {file.create("00-rawdata/README.md")}
 if(!dir.exists("01-scripts")) { dir.create("01-scripts")}
 if(!dir.exists("01-scripts/r_scripts")) { dir.create("01-scripts/r_scripts")}
-if(!dir.exists("01-scripts/RMD")) { dir.create("01-scripts/RMD")}
+if(!dir.exists("01-scripts/RMD_scripts")) { dir.create("01-scripts/RMD_scripts")}
 if(!dir.exists("02-outdata")) { dir.create("02-outdata")}
 if(!dir.exists("03-figs")) { dir.create("03-figs")}
 if(!dir.exists("04-manuscript")) { dir.create("04-manuscript")}
 if(!dir.exists("04-manuscript/rendered")) { dir.create("04-manuscript/rendered")}
 
-if(!file.exists("00-rawdata/README.MD")) {file.create("00-rawdata/README.md")}
-if(!file.exists("01-scripts/README.MD")) { file.create("01-scripts/README.MD")}
-if(!file.exists("01-scripts/r_scripts/README.MD")) { file.create("01-scripts/r_scripts/README.MD")}
-if(!file.exists("01-scripts/RMD/README.MD")) { file.create("01-scripts/RMD/README.MD")}
-if(!file.exists("02-outdata/README.MD")) { file.create("02-outdata/README.MD")}
-if(!file.exists("03-figs/README.MD")) { file.create("03-figs/README.MD")}
-if(!file.exists("04-manuscript/README.MD")) { file.create("04-manuscript/README.MD")}
-if(!file.exists("04-manuscript/rendered/README.MD")) { file.create("04-manuscript/rendered/README.MD")}
+if(!file.exists("00-rawdata/_README.MD")) {file.create("00-rawdata/_README.MD")}
+if(!file.exists("00-rawdata/DATA-DICTIONARY.md")) {file.create("00-rawdata/DATA-DICTIONARY.md")}
+if(!file.exists("01-scripts/_README.MD")) { file.create("01-scripts/_README.MD")}
+if(!file.exists("01-scripts/r_scripts/_README.MD")) { file.create("01-scripts/r_scripts/_README.MD")}
+if(!file.exists("01-scripts/RMD/_README.MD")) { file.create("01-scripts/RMD/_README.MD")}
+if(!file.exists("02-outdata/_README.MD")) { file.create("02-outdata/_README.MD")}
+if(!file.exists("03-figs/_README.MD")) { file.create("03-figs/_README.MD")}
+if(!file.exists("04-manuscript/_README.MD")) { file.create("04-manuscript/_README.MD")}
+if(!file.exists("04-manuscript/my-library.bib")) { file.create("04-manuscript/my-library.bib")}
+if(!file.exists("04-manuscript/manuscript.RMD")) { file.create("04-manuscript/manuscript.RMD")}
+if(!file.exists("04-manuscript/rendered/_README.MD")) { file.create("04-manuscript/rendered/_README.MD")}
 
 # file.remove("create_project_structure.R")
